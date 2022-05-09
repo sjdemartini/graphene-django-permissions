88d19a6a604c4c67d14c8f7d678e5532a444ebfd
Steven DeMartini <sjdemartini@users.noreply.github.com>
2022-05-09 10:17:41 -0700

    Remove generated clutter in tests

--- tests/test_graphene_django_permissions.py
+++ tests/test_graphene_django_permissions.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python
-"""Tests for `graphene_django_permissions` package."""
-
 import pytest
-
 
 
 @pytest.fixture
@@ -19,4 +15,3 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
-    del response
