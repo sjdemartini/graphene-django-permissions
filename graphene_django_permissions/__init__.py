77b9ad18640c610dd4cd1dbc4463da3fabda5296
Steven DeMartini <sjdemartini@users.noreply.github.com>
2022-05-09 08:43:57 -0700

    Remove unneeded initial file content

--- graphene_django_permissions/__init__.py
+++ graphene_django_permissions/__init__.py
@@ -1,5 +1,2 @@
-"""Top-level package for Graphene Django Permissions."""
-
 __author__ = """Steven DeMartini"""
-__email__ = 'sjdemartini@users.noreply.github.com'
-__version__ = '0.1.0'
+__version__ = "0.1.0"
